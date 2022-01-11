9edc7d5a317457b3c15c49ac8f36f52117c0704a
Thorson <thorsonb@nala.roche.com>
2022-01-11 15:45:09 -0800

    add db

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://root:root@cluster0.5i0fa.mongodb.net/artforyourheart?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
