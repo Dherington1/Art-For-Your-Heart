64881cf2a4216df483139167d88794ee20be27d9
Dherington1 <dherington1@comcast.net>
2023-07-03 21:37:14 -0700

    building app

---
+++ client/build/precache-manifest.5d61f264124cf6d457be2eee64de5a26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "962a7334bf08ed743626c18670b32fa3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70c1c53d58937b58d32a",
+    "url": "/static/css/2.171a7357.chunk.css"
+  },
+  {
+    "revision": "2298d328cc81ddfbd38f",
+    "url": "/static/css/main.51adc30e.chunk.css"
+  },
+  {
+    "revision": "70c1c53d58937b58d32a",
+    "url": "/static/js/2.461f5f8f.chunk.js"
+  },
+  {
+    "revision": "bf1f424833d5d41c5dbfe8efe39dbf20",
+    "url": "/static/js/2.461f5f8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2298d328cc81ddfbd38f",
+    "url": "/static/js/main.564adaf7.chunk.js"
+  },
+  {
+    "revision": "e440b1b8f446010c513a",
+    "url": "/static/js/runtime-main.23c22a85.js"
+  }
+]);
